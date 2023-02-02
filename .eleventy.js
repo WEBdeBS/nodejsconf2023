f892f2fcde353306bf80b48279ca8a25ac5fc5f2
Antonio Molinari <molinari@incode.it>
2023-02-02 22:54:31 +0100

    wip eleventy

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addPassthroughCopy('js')
+  eleventyConfig.addPassthroughCopy('assets')
+  return {
+    passthroughFileCopy: true
+  }
+}
