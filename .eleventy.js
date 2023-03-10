77372facda363eac74591fe02db2d9ed9a7dc246
Antonio Molinari <molinari@incode.it>
2023-03-11 00:18:09 +0100

    wip

--- .eleventy.js
+++ .eleventy.js
@@ -3,11 +3,32 @@
 const markdownIt = require("markdown-it");
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
+const isDev = process.env.ELEVENTY_ENV === 'development';
+const site = {
+  url: process.env.URL || "http://localhost:8080",
+  title: "The Node.js Italian Conference 2023",
+  description: "The Italian Nodejsconf is back!",
+  image: "/assets/fb_banner.png",
+  twitter: "webdebresa",
+}
+
+/**
+ * Prefixes the given URL with the site's base URL.
+ * @param {string} url
+ */
+const toAbsoluteUrl = (url) => {
+  return new URL(url, site.url).href;
+}
+
+
+
 module.exports = function(config) {
   config.addPassthroughCopy('src/assets')
   config.addWatchTarget("./src/");
   config.addWatchTarget("./src/scss");
   config.addLayoutAlias("layout1", "layout1.njk");
+
+  config.addFilter('toAbsoluteUrl', toAbsoluteUrl);
 
   config.addPlugin(EleventyRenderPlugin);
 
@@ -37,8 +58,9 @@ module.exports = function(config) {
       input: "src",
       output: "dist",
       includes: "includes",
-      data: "data",
-          },
+      //data: "data", // relative to input
+    },
+    site: {},
     templateFormats: ["html", "njk", "md", "11ty.js"],
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
