fc00ea0890e2bd190ff4263fa8c85a7d27ae63ae
Antonio Molinari <molinari@incode.it>
2023-05-13 11:29:05 +0200

    .md files in /contents folder

--- .eleventy.js
+++ .eleventy.js
@@ -13,12 +13,12 @@ module.exports = function(config) {
   config.addPlugin(EleventyRenderPlugin);
 
   config.addCollection("talks",(collection) => {
-    return collection.getFilteredByGlob("./src/content/talks/*.md");
+    return collection.getFilteredByGlob("./src/contents/talks/*.md");
   });
 
   config.addCollection("sponsor_packages",(collection) => {
     return collection
-    .getFilteredByGlob("./src/content/sponsor_packages/*.md")
+    .getFilteredByGlob("./src/contents/sponsor_packages/*.md")
     .sort((a, b) => {
       return a.data.weight - b.data.weight;
     })
